edf53ecb6740678006ab7c980d5c98b63c63d1b6
chris <chriswilker@hotmail.com>
2020-04-07 00:42:12 -0700

    Initial commit

---
+++ proportions.py
@@ -0,0 +1,67 @@
+import argparse
+import yaml
+from typing import Dict
+import urllib.request
+import json
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "portfolio_file", type=str, help="yaml file representing a portfolio"
+    )
+    args = parser.parse_args()
+
+    with open(args.portfolio_file, "r") as stream:
+        try:
+            portfolio = yaml.safe_load(stream)
+            print(market_proportions_output(portfolio), end="")
+        except yaml.YAMLError as e:
+            print(e)
+
+
+def market_proportions_output(portfolio: Dict) -> str:
+    mps = market_proportions(portfolio)
+    output = ""
+    for asset in mps.keys():
+        mp = mps[asset]
+        market_percent = mp * 100
+        output += f"{asset}: {market_percent:.2f}%\n"
+    return output
+
+
+def market_proportions(portfolio: Dict) -> Dict:
+    mcs = market_caps(portfolio)
+    sum_mcs = sum([mcs[asset] for asset in mcs.keys()])
+    mps = {}
+    for asset in mcs.keys():
+        mps[asset] = mcs[asset] / sum_mcs
+    return mps
+
+
+def market_caps(portfolio: Dict) -> Dict:
+    mcs = {}
+    for asset in portfolio.keys():
+        mcs[asset] = market_cap(portfolio[asset])
+    return mcs
+
+
+def market_cap(asset: Dict) -> float:
+    return asset["market cap"] * (
+        current_price(asset["ticker"]) / asset["close price"]
+    )
+
+
+def current_price(ticker: str) -> float:
+    url = f"https://query1.finance.yahoo.com/v7/finance/quote?symbols={ticker}"
+    contents = urllib.request.urlopen(url).read()
+    response = json.loads(contents)["quoteResponse"]
+    error = response["error"]
+    if error:
+        raise ValueError(f"Response from {url} contains an error:\n{error}")
+    else:
+        return response["result"][0]["regularMarketPrice"]
+
+
+if __name__ == "__main__":
+    main()
